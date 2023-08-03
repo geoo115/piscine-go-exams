60727c17705e38dfca5717f3cef9fdc761b22b39
George saad <127685842+geoo115@users.noreply.github.com>
2023-08-04 00:01:45 +0100

    Create reduceint.go

---
+++ reduceint.go
@@ -0,0 +1,14 @@
+package piscine
+
+func ReduceInt(a []int, f func(int, int) int) {
+	var result int
+	for i := 0; i < len(a)-1; i++ {
+		if i == 0 {
+			result = a[0]
+		}
+		result = f(result, a[i+1])
+	}
+	for _, char := range strconv.Itoa(result) {
+		z01.PrintRune(rune(char))
+	}
+}
