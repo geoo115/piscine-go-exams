d3a62eb219afe7242c5dd8691b858047a748ac9b
George saad <127685842+geoo115@users.noreply.github.com>
2023-08-03 15:58:15 +0100

    Create hello.go

---
+++ hello.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	z01.PrintRune('H')
+	z01.PrintRune('e')
+	z01.PrintRune('l')
+	z01.PrintRune('l')
+	z01.PrintRune('o')
+	z01.PrintRune(' ')
+	z01.PrintRune('W')
+	z01.PrintRune('o')
+	z01.PrintRune('r')
+	z01.PrintRune('l')
+	z01.PrintRune('d')
+	z01.PrintRune('!')
+	z01.PrintRune('\n')
+}
