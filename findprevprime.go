b504fd4a3020efa0119962bd7fd3e0e5e7362c53
George saad <127685842+geoo115@users.noreply.github.com>
2023-08-04 21:26:13 +0100

    Create findprevprime.go

---
+++ findprevprime.go
@@ -0,0 +1,22 @@
+package piscine
+
+func FindPrevPrime(nb int) int {
+	for i := nb; i >= 2; i-- {
+		if isPrime(i) {
+			return i
+		}
+	}
+	return 0
+}
+
+func isPrime(num int) bool {
+	if num <= 1 {
+		return false
+	}
+	for i := 2; i*i <= num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
