c2f52c6203015678e93be2b8ba471ea183b34ff5
George saad <127685842+geoo115@users.noreply.github.com>
2023-08-04 13:37:23 +0100

    Create searchreplace.go

---
+++ searchreplace.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	if len(os.Args) != 4 {
+		return
+	}
+	a := os.Args[1]
+	b := os.Args[2]
+	c := os.Args[3]
+	var result string
+	for _, ch := range a {
+		if ch == rune(b[0]) {
+			ch = rune(c[0])
+			result += string(ch)
+		} else {
+			result += string(ch)
+		}
+
+	}
+	for _, r := range result {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
