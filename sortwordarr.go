e02647523a865be327da32c8f95d7a0fb7e25c05
George saad <127685842+geoo115@users.noreply.github.com>
2023-08-06 19:47:04 +0100

    Create sortwordarr.go

---
+++ sortwordarr.go
@@ -0,0 +1,16 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func SortWordArr(array []string) {
+	n := len(array)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+}
