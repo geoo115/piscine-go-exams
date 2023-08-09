7e67656c7445701a4c7c58e89a2a143ec6206f5d
George saad <127685842+geoo115@users.noreply.github.com>
2023-08-09 21:02:00 +0100

    Create split.go

---
+++ split.go
@@ -0,0 +1,17 @@
+package piscine
+
+func Split(s, sep string) []string {
+	var result []string
+	start := 0
+	for i := 0; i < len(s); i++ {
+		if i+len(sep) <= len(s) && s[i:i+len(sep)] == sep {
+			result = append(result, s[start:i])
+			start = i + len(sep)
+			i += len(sep) - 1
+		}
+	}
+	if start < len(s) {
+		result = append(result, s[start:])
+	}
+	return result
+}
